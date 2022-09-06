414d6260c51f62e03a600742aba03b1b1d38a653
SrikarGathey1 <venkatnaras123@gmail.com>
2022-09-06 13:05:49 +0530

    Some changes to database.py

--- src/database.py
+++ src/database.py
@@ -15,18 +15,41 @@ curs = conn.cursor()
 #            dob number )
 #            """)
 
+
+
+# Creating States and Numbers table.
+# curs.execute("CREATE TABLE states(stateID number PRIMARY KEY, name text)")
+
+# Inserting values in States Table.
+# while 1:
+#    stateID = int(input("Enter State id:"))
+#    name = input("Enter state name:")
+#    curs.execute("INSERT INTO states VALUES(?, ?)", (stateID, name))
+#    flag = int(input("Do you wish to continue?"))
+#    if flag == 1:
+#        break
+#    else:
+#        continue
+
+# curs.execute("SELECT * FROM states")
+# print(curs.fetchall())
+
 # Adding a record
 # curs.execute("INSERT INTO patients VALUES('00117000927', 'Srikar Gade', 'venkatnaras123@gmail.com', 1, 17)")
 # curs.execute("INSERT INTO patients VALUES('00817004671', 'John Brown', 'john.brown@gmail.com', 8, 17)")
 # curs.execute("INSERT INTO patients VALUES('02409003871', 'Matt Smith', 'matt.smith1@gmail.com', 24, 9)")
  
 
-# Displaying the table
-curs.execute("SELECT * FROM patients")
+# Displaying the table patients
+# curs.execute("SELECT rowid, * FROM patients")
+# print(curs.fetchall())
 # print("Unique ID\t\t", "Name\t\t", "Email\t\t", "State\t\t", "Date Of Birth")
 # for item in curs.fetchall():
 #    print(item[0], "\t\t", item[1], "\t", item[2], item[3], item[4])
 
+# Fetching Name of Patient and the State they are from.
+#curs.execute("SELECT p.name, s.name FROM patients p, states s where s.stateID = p.state")
+# print(curs.fetchall())
 
 # Commiting our command
 conn.commit()
